80efefca5d70215bd8e7ef700d8c63b0d5b02a44
irdi1 <irdielezi0@gmail.com>
2025-03-08 21:14:27 +0100

    UrlMapping entity and JPA repository

---
+++ src/main/java/com/urlshortner/urlshortnerapi/model/UrlMapping.java
@@ -0,0 +1,44 @@
+package com.urlshortner.urlshortnerapi.model;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "url_mapping")
+public class UrlMapping {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String originalUrl;
+
+    @Column(unique = true)
+    private String shortCode;
+
+    public UrlMapping() { }
+
+    public UrlMapping(String originalUrl) {
+        this.originalUrl = originalUrl;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getOriginalUrl() {
+        return originalUrl;
+    }
+
+    public String getShortCode() {
+        return shortCode;
+    }
+
+    public void setOriginalUrl(String originalUrl) {
+        this.originalUrl = originalUrl;
+    }
+
+    public void setShortCode(String shortCode) {
+        this.shortCode = shortCode;
+    }
+}
