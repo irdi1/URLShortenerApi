95f026daddd63e20c864ff626a4b180e6906d6e8
irdi1 <irdielezi0@gmail.com>
2025-03-08 21:05:09 +0100

    Intial project Setup

---
+++ src/main/java/com/urlshortner/urlshortnerapi/UrlshortnerapiApplication.java
@@ -0,0 +1,13 @@
+package com.urlshortner.urlshortnerapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UrlshortnerapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UrlshortnerapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/urlshortner/urlshortnerapi/UrlshortnerapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.urlshortner.urlshortnerapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UrlshortnerapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
